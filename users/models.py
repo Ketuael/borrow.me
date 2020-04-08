5f464c14d012a43c1155515a48ef1e393f35f785
Ketuael <ketuael@gmail.com>
2020-04-09 01:21:23 +0200

    First implementation of custom User account (with serializer and view+url pathing) -> just for tests, will be modified

--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+
+    USERNAME_FIELD = 'email'
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from users.models import User
+from users.serializers import UserSerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
