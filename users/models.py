7fe05533b899cc616e3642a74c8e8c2746a9272a
Ketuael <ketuael@gmail.com>
2020-04-09 16:12:05 +0200

    User Model - added 'avatar' attribute (ImageField) along with required settings / url handlers

--- users/models.py
+++ users/models.py
@@ -7,8 +7,11 @@ from django.contrib.auth.models import AbstractBaseUser
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
 
     USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
 
 
--- users/views.py
+++ users/views.py
@@ -6,9 +6,6 @@ from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list` and `detail` actions.
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
