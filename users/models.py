25941512ddff1dede6b54367dc3761590451d553
Ketuael <ketuael@gmail.com>
2020-04-09 17:42:02 +0200

    Slightly improved User model (added UserManager); needs rework

--- users/models.py
+++ users/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if email == "":
+            raise ValueError('Musisz podac adres email')
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser):
@@ -10,7 +21,11 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length=50, blank=False)
     avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     is_anonymous = False
     is_authenticated = True
 
