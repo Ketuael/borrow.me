4767982eb35deb869490342bea5e7f3513db7ade
Ketuael <ketuael@gmail.com>
2020-04-16 00:59:20 +0200

    Changed MEDIA_ROOT to /media/; changed avatars directory to user_$id/avatars/$name

--- users/models.py
+++ users/models.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/avatars/{1}'.format(instance.id, filename)
+
+
 class UserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, avatar, password=None):
+    def create_user(self, email, first_name, last_name, avatar=None, password=None):
         if not email:
             raise ValueError('Musisz podac adres email')
         email = self.normalize_email(email)
@@ -19,7 +23,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
-    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    avatar = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
 
     objects = UserManager()
 
