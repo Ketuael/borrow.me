3797cbf033606ab046c1fdfed4f814d68a49c6e7
Ketuael <ketuael@gmail.com>
2020-04-23 20:29:26 +0200

    Fixed typo in urls

--- backend/urls.py
+++ backend/urls.py
@@ -36,7 +36,7 @@ urlpatterns += [
 
 
 urlpatterns += [
-    path(friends_root_url + '/friends/', friend_views.FriendshipListView.as_view(), name='friend-list'),
+    path(friends_root_url + '/', friend_views.FriendshipListView.as_view(), name='friend-list'),
     path(friends_root_url + '/add', friend_views.AddFriendView.as_view(), name='friend-add'),
     path(friends_root_url + '/<int:pk>/', friend_views.FriendshipDetailView.as_view(), name='friend-detail'),
     path(friends_root_url + '/<int:pk>/confirmation', friend_views.ConfirmFriendshipView.as_view(), name='friend-confirmation'),
