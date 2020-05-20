0591553b900218268af391e29d5e6733b3976a09
Adrian Murach <adrian.murach95@gmail.com>
2020-05-20 17:15:23 +0000

    Added permissions and filtering for TransactionListView

--- transaction/views.py
+++ transaction/views.py
@@ -10,6 +10,13 @@ class TransactionsListView(generics.ListAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionsListSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        giver = Transaction.objects.filter(giver=user)
+        taker = Transaction.objects.filter(taker=user)
+        return giver.union(taker)
+
+    permission_classes = [permissions.IsAuthenticated]
 
 class CreateTransactionView(generics.CreateAPIView):
     queryset = Transaction.objects.all()
